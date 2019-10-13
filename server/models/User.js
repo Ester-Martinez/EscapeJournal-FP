d162abb5edca90cc18da35820a0fe0f4dd8ae464
Ester-Martinez <martinez.ester@gmail.com>
2019-10-13 18:04:58 +0200

    last working point

--- server/models/User.js
+++ server/models/User.js
@@ -15,7 +15,7 @@ const userSchema = new Schema(
       unique: true,
       lowercase: true,
       trim: true,
-      match: [EMAIL_PATTERN, "this is not a correct email"]
+      // match: [EMAIL_PATTERN, "this is not a correct email"]
     },
     userType: {
       type: String,
