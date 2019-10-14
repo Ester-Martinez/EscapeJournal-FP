0bb01374584c7003f64bae4d7c26aa4b67c7564e
Ester-Martinez <martinez.ester@gmail.com>
2019-10-14 12:28:38 +0200

    props enviadas

--- client/src/components/contents/Contents.jsx
+++ client/src/components/contents/Contents.jsx
@@ -1,27 +1,84 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import HomeLogged from './HomeLogged';
-import AddExperience from './AddExperience';
-import AllExperiences from './AllExperiences';
-import Profile from './Profile';
-import EscapeService from "./components/contents/EscapeService";
+import HomeLogged from "./HomeLogged";
+import AddExperience from "./AddExperience";
+import AllExperiences from "./AllExperiences";
+import Profile from "./Profile";
+import EscapeService from "./EscapeService";
 
 class Contents extends Component {
   constructor(props) {
     super(props);
-    this.state = { loggedInUser: this.props.userInSession };
-    this.service = new EscapeService();
+    this.state = {
+      loggedInUser: this.props.userInSession,
+      escapes: [],
+      rooms: []
+    };
+    this.userService = new EscapeService();
+  }
+  getEscapes() {
+    this.userService.allEscapes().then(response => {
+      this.setState({
+        ...this.state,
+        escapes: response
+      });
+    });
+  }
+  getRooms() {
+    this.userService.allRooms().then(response => {
+      this.setState({
+        ...this.state,
+        rooms: response
+      });
+    });
+  }
+  componentDidMount() {
+    this.getEscapes();
+    this.getRooms();
   }
   render() {
     return (
       <Switch>
-      <Route exact path="/home" render={() => <HomeLogged />} />
-      <Route exact path="/add-experience" render={() => <AddExperience />} />
-      <Route exact path="/all-experiences" render={() => <AllExperiences  />} />
-      <Route exact path="/profile" render={() => <Profile />} />
-    </Switch>
-      );
-    
+        <Route
+          exact
+          path="/home"
+          render={() => (
+            <HomeLogged
+              escapes={this.state.escapes}
+              rooms={this.state.rooms}
+              user={this.state.loggedInUser}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/add-experience"
+          render={() => (
+            <AddExperience
+              escapes={this.state.escapes}
+              rooms={this.state.rooms}
+              user={this.state.loggedInUser}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/all-experiences"
+          render={() => (
+            <AllExperiences
+              escapes={this.state.escapes}
+              rooms={this.state.rooms}
+              user={this.state.loggedInUser}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/profile"
+          render={() => <Profile user={this.state.loggedInUser} />}
+        />
+      </Switch>
+    );
   }
 }
 
--- client/src/components/contents/EscapeService.js
+++ client/src/components/contents/EscapeService.js
@@ -8,11 +8,18 @@ class EscapeService {
     });
   }
 
-  addExperience = (username, password) => {
-    return this.service.post('//add-experience', {username, password})
+  addExperience = (escapeDone, roomsDone, team, date, imgName, imgPath) => {
+    return this.service.post('//add-experience', {escapeDone, roomsDone, team, date, imgName, imgPath})
     .then(response => response.data)
   }
-
+  allEscapes = () => {
+    return this.service.get('/allescapes')
+    .then(response => response.data)
+  }
+  allRooms = () => {
+    return this.service.get('/allrooms')
+    .then(response => response.data)
+  }
 }
 
 export default EscapeService;
