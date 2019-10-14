7ecb322f7fbb08df692df8d38350e9cde93c863c
Ester-Martinez <martinez.ester@gmail.com>
2019-10-14 18:01:00 +0200

    first deploy

--- server/app.js
+++ server/app.js
@@ -63,7 +63,7 @@ app.use(
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 hbs.registerHelper("ifUndefined", (value, options) => {
   if (arguments.length < 2)
