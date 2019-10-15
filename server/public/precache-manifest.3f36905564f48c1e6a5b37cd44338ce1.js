8c846bfddeb9f05c3d156f4f6027381861e3209e
Ester-Martinez <martinez.ester@gmail.com>
2019-10-15 09:14:12 +0200

    arreglado

---
+++ server/public/precache-manifest.3f36905564f48c1e6a5b37cd44338ce1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbf634a3058b4d9a000a8b2e89eb10c6",
+    "url": "/static/media/journal-LOGO2.cbf634a3.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5a837a8888dbb923e5f7",
+    "url": "/static/js/main.5a837a88.chunk.js"
+  },
+  {
+    "revision": "1afc21599bc7563b5853",
+    "url": "/static/js/1.1afc2159.chunk.js"
+  },
+  {
+    "revision": "5a837a8888dbb923e5f7",
+    "url": "/static/css/main.c5493647.chunk.css"
+  },
+  {
+    "revision": "1afc21599bc7563b5853",
+    "url": "/static/css/1.b2423a93.chunk.css"
+  },
+  {
+    "revision": "25feb4cffa942f961924d71e593fe1aa",
+    "url": "/index.html"
+  }
+];
