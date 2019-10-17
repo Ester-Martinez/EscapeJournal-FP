c968f34a9b609001cd9a7f1a92cdb24477ace0f1
Ester-Martinez <martinez.ester@gmail.com>
2019-10-17 13:50:06 +0200

    added share buttons - heroku

---
+++ server/public/precache-manifest.7015d8f8050da816804bf93e1a837bc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbf634a3058b4d9a000a8b2e89eb10c6",
+    "url": "/static/media/journal-LOGO2.cbf634a3.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "663ee168a608ff3c665c",
+    "url": "/static/js/main.663ee168.chunk.js"
+  },
+  {
+    "revision": "3534cd0d7a2809f44c24",
+    "url": "/static/js/1.3534cd0d.chunk.js"
+  },
+  {
+    "revision": "663ee168a608ff3c665c",
+    "url": "/static/css/main.35ca6e2c.chunk.css"
+  },
+  {
+    "revision": "3534cd0d7a2809f44c24",
+    "url": "/static/css/1.521c8ba3.chunk.css"
+  },
+  {
+    "revision": "e3e234a8c86ba8b4488382ec97dfa306",
+    "url": "/index.html"
+  }
+];
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e830019222c611f4952f2c0ec8068f8f.js"
+  "/precache-manifest.7015d8f8050da816804bf93e1a837bc4.js"
 );
 
 workbox.clientsClaim();
